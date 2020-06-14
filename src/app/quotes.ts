2d8086e766f03b65938b0e89c2630d9f549ad8cd
alex <alexkimutai021@gmail.com>
2020-06-14 13:29:05 +0300

    quotes html

---
+++ src/app/quotes.ts
@@ -0,0 +1,7 @@
+
+
+export class Quotes {
+    constructor (public names:string, public quote:string, public email:string, public numberOfUpVotes:number=0, public numberOfDownVotes:number=0,public timing: Date){
+
+    }
+}
